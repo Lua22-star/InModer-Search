62370b8b922ef72bb9742b3e22c8423e03c8996e
Lua22-star <mysha8599@gmail.com>
2026-08-09 20:35:46 +0200

    Add search form to index.php

---
+++ phps/index.php
@@ -0,0 +1,12 @@
+<!-- index.php -->
+<search>
+  <form action="search.php" method="GET">
+    <input 
+      type="search" 
+      name="q" 
+      placeholder="Search products or articles..." 
+      required
+    >
+    <button type="submit">Search</button>
+  </form>
+</search>
